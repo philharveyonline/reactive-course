6c6920a214212b5957a0fd7b8759310524fb5582
Phil Harvey <phil@philharveyonline.com>
2013-11-17 23:31:34 +0000

    got week two epidemy simulation working. Should be able to submit after some tidying up

--- simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -11,14 +11,71 @@ class EpidemySimulator extends Simulator {
     val roomRows: Int = 8
     val roomColumns: Int = 8
 
-    // to complete: additional parameters of simulation
+    val prevalenceRate = 0.01
+    val infectionRate = 0.4
+    val deathRate = 0.25
+
+    val maxTimeBetweenMoves = 5
+
+    val incubationPeriod = 6
+    val deathPeriod = 14
+    val immunePeriod = 16
+    val healthyPeriod = 18
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  private def generatePersonList(): List[Person] = {
+    val people = (1 to population) map { id =>
+      val p = new Person(id)
+      if (id <= population * prevalenceRate) {
+        p.infect()
+      }
+      afterDelay(0)(p.move())
+      p
+    }
+    people.toList
+  }
 
-  class Person (val id: Int) {
+  val persons: List[Person] = generatePersonList()
+
+  case class Room(row: Int, col: Int) {
+
+    private def wrap(x: Int, numValues: Int) = {
+      x match {
+        case -1 => numValues - 1 // wrap under
+        case `numValues` => 0 // wrap over
+        case okValue => okValue
+      }
+    }
+
+    def containsSickOrDead(): Boolean = {
+      persons exists { person =>
+        this == person.room && person.sickOrDead
+      }
+    }
+
+    def containsInfectious(): Boolean = {
+      persons exists { person =>
+        this == person.room && person.infected
+      }
+    }
+
+    def adjacentRooms(): List[Room] = {
+      val left = wrap(col - 1, roomColumns)
+      val right = wrap(col + 1, roomColumns)
+      val up = wrap(row - 1, roomRows)
+      val down = wrap(row + 1, roomRows)
+
+      List(
+        Room(row, left),
+        Room(row, right),
+        Room(up, col),
+        Room(down, col)) filterNot (room => room.containsSickOrDead)
+    }
+  }
+
+  class Person(val id: Int) {
     var infected = false
     var sick = false
     var immune = false
@@ -28,8 +85,69 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    def room() = Room(row, col)
+    def setRoom(room: Room) = {
+      row = room.row
+      col = room.col
+    }
+
+    def sickOrDead(): Boolean = {
+      sick || dead
+    }
+
+    def infect(): Unit = {
+      infected = true
+      afterDelay(incubationPeriod) {
+        sick = true
+      }
+      afterDelay(deathPeriod) {
+        if (random < deathRate) {
+          dead = true
+        }
+      }
+      afterDelay(immunePeriod) {
+        if (!dead) {
+          sick = false
+          immune = true
+        }
+      }
+      afterDelay(healthyPeriod) {
+        if (!dead) {
+          infected = false
+          sick = false
+          immune = false
+        }
+      }
+    }
+
+    def maybeInfect(): Unit = {
+      if (infected || dead || immune) {
+        // we can't become infected
+      } else {
+        if (room.containsInfectious) {
+          if (random < infectionRate) {
+            infect
+          }
+        }
+      }
+    }
+
+    /**
+     * Move now, and schedule another move later
+     */
+    protected[simulations] def move(): Unit = {
+      if (!dead) {
+        val adjacentRooms = room.adjacentRooms
+        if (!adjacentRooms.isEmpty) {
+          val nextRoomIndex = randomBelow(adjacentRooms.size)
+          val nextRoom = adjacentRooms(nextRoomIndex)
+          setRoom(nextRoom)
+          maybeInfect()
+        }
+        val timeUntilNextMove = randomBelow(maxTimeBetweenMoves + 1)
+
+        afterDelay(timeUntilNextMove) { move() }
+      }
+    }
   }
 }
